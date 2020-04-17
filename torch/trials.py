fa7207973173d76122d4d899e907588cb626337e
SaumilShah66 <shah.saumil66@gmail.com>
2020-04-16 21:17:00 -0400

    Initial folders

---
+++ torch/trials.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+from torch.nn.parameter import Parameter
+from torch.nn import functional as F
+from torch.nn.modules.conv import _ConvNd
+from torch.nn.modules.conv import _ConvTransposeMixin
+from torch.nn.modules.utils import _pair
+
+
