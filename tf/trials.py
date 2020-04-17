7a2b17c18fa76671c230ef17e3555f444d94e350
Varun <varunasthana92@gmail.com>
2020-04-17 15:18:07 -0400

    adf.LeakyReLU added as tf and its test

--- tf/trials.py
+++ tf/trials.py
@@ -83,5 +83,34 @@ class ReLU(tf.keras.Model):
             output_variance = self._keep_variance_fn(output_variance)
         return output_mean, output_variance
 
+class LeakyReLU(tf.keras.Model):
+    def __init__(self, negative_slope=0.01, keep_variance_fn=None):
+        super(LeakyReLU, self).__init__()
+        self._keep_variance_fn = keep_variance_fn
+        self._negative_slope = negative_slope
+
+    def call(self, features_mean, features_variance):
+        features_stddev = tf.cast(tf.sqrt(features_variance), tf.float64)
+        div = features_mean / features_stddev
+        pdf = normpdf(div)
+        cdf = normcdf(div)
+        negative_cdf = 1.0 - cdf
+        mu_cdf = features_mean * cdf
+        stddev_pdf = features_stddev * pdf
+        squared_mean_variance = features_mean ** 2 + features_variance
+        mean_stddev_pdf = features_mean * stddev_pdf
+        mean_r = mu_cdf + stddev_pdf
+        variance_r = squared_mean_variance * cdf + mean_stddev_pdf - mean_r ** 2
+        mean_n = - features_mean * negative_cdf + stddev_pdf
+        variance_n = squared_mean_variance * negative_cdf - mean_stddev_pdf - mean_n ** 2
+        covxy = - mean_r * mean_n
+        outputs_mean = mean_r - self._negative_slope * mean_n
+        outputs_variance = variance_r \
+                           + self._negative_slope * self._negative_slope * variance_n \
+                           - 2.0 * self._negative_slope * covxy
+        if self._keep_variance_fn is not None:
+            outputs_variance = self._keep_variance_fn(outputs_variance)
+        return outputs_mean, outputs_variance
+
 
 
