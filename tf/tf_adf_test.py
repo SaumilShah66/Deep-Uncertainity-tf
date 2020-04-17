97b4365b691133299953025aad9aeb5e2a9f97b0
SaumilShah66 <shah.saumil66@gmail.com>
2020-04-16 22:50:38 -0400

    tensorflow avePool2D test added

---
+++ tf/tf_adf_test.py
@@ -0,0 +1,21 @@
+import tensorflow as tf 
+from trials import *
+import numpy as np 
+
+def avgPool2dTest(mean, variance, pool_size):
+	AvgPool2d_ = AvgPool2d()
+	l = AvgPool2d_(mean, variance, pool_size)
+	with tf.Session() as sess:
+		outMean, outVar = sess.run(l)
+	print("Mean " + "-" * 20)
+	print(outMean)
+	print("Variance " + "-" * 20)
+	print(outVar)
+	pass
+
+mean_ = np.array([[1,  2,  3,  4], [5,  6,  7,  8], [9, 10, 11, 12], [13, 14, 15, 16]], dtype=np.float64)
+mean_ = np.reshape(mean_, (1,4,4,1))
+mean = tf.convert_to_tensor(mean_)
+variance = tf.zeros_like(mean) + 0.001
+pool_size = (2, 2)
+avgPool2dTest(mean, variance, pool_size)
