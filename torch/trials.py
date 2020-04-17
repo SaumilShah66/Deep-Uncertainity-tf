12b74ed7ed52804e00f42998c408cabbdb26c701
SaumilShah66 <shah.saumil66@gmail.com>
2020-04-17 15:15:55 -0400

    Test file for leakyrelu pytorch

--- torch/trials.py
+++ torch/trials.py
@@ -84,5 +84,35 @@ class ReLU(nn.Module):
             outputs_variance = self._keep_variance_fn(outputs_variance)
         return outputs_mean, outputs_variance
 
+class LeakyReLU(nn.Module):
+    def __init__(self, negative_slope=0.01, keep_variance_fn=None):
+        super(LeakyReLU, self).__init__()
+        self._keep_variance_fn = keep_variance_fn
+        self._negative_slope = negative_slope
+
+    def forward(self, features_mean, features_variance):
+        features_stddev = torch.sqrt(features_variance)
+        div = features_mean / features_stddev
+        pdf = normpdf(div)
+        cdf = normcdf(div)
+        negative_cdf = 1.0 - cdf
+        mu_cdf = features_mean * cdf
+        stddev_pdf = features_stddev * pdf
+        squared_mean_variance = features_mean ** 2 + features_variance
+        mean_stddev_pdf = features_mean * stddev_pdf
+        mean_r = mu_cdf + stddev_pdf
+        variance_r = squared_mean_variance * cdf + mean_stddev_pdf - mean_r ** 2
+        mean_n = - features_mean * negative_cdf + stddev_pdf
+        variance_n = squared_mean_variance * negative_cdf - mean_stddev_pdf - mean_n ** 2
+        covxy = - mean_r * mean_n
+        outputs_mean = mean_r - self._negative_slope * mean_n
+        outputs_variance = variance_r \
+                           + self._negative_slope * self._negative_slope * variance_n \
+                           - 2.0 * self._negative_slope * covxy
+        if self._keep_variance_fn is not None:
+            outputs_variance = self._keep_variance_fn(outputs_variance)
+        return outputs_mean, outputs_variance
+
+
 
 # if __name__ == "__main__":
